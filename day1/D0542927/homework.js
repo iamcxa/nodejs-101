5978ca560f6b8afa9f3bf4c4864b0ffba43dd759
fcu-d0542927 <32700005+fcu-d0542927@users.noreply.github.com>
2017-12-26 19:52:27 +0800

    Create homework.js

---
+++ day1/D0542927/homework.js
@@ -0,0 +1,23 @@
+{
+  "name": "homework",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "echo '執行 start' ",
+    "test": "echo '執行 test'",
+    "preinstall": "echo '執行 preinstall'",
+    "install": "echo '執行 install'",
+    "postinstall": "echo '執行 postinstall'",
+    "helloworld": "echo helloworld"
+  },
+  "author": "D0542927",
+  "license": "ISC",
+  "dependencies": {
+    "axios": "^0.17.1"
+  },
+  "devDependencies": {
+    "mocha": "^4.0.1",
+    "nodemon": "^1.14.1"
+  },
+  "description": ""
+}
