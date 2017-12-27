7102a8b0670dc5d46a9617c5558a7190370fcc3d
fcu-d0341607 <33278404+fcu-d0341607@users.noreply.github.com>
2017-12-27 16:22:19 +0800

    good
    good

---
+++ day1/d0341607/pratice.js
@@ -0,0 +1,18 @@
+{
+  "name": "d0341607",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+	"homework": "Hello World"
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "axios": "^0.17.1"
+  },
+  "devDependencies": {
+    "mocha": "^4.0.1"
+  }
+}
