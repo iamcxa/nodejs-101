ab60f3efb5e5ab1b9bdae54e17512fa2483f9812
fcu-d0542694 <32485314+fcu-d0542694@users.noreply.github.com>
2018-01-02 16:30:24 +0800

    Add files via upload

---
+++ day2/practice2/loadash.js
@@ -0,0 +1,17 @@
+﻿var fs = require('fs');
+var _ = require('lodash');
+var log = require('./log.js');
+
+var result;
+
+log.time('timer');
+
+fs.readFile('./data.json', function (err, data) {
+    if (err) throw err;
+    data = JSON.parse(data);
+    result = _.filter(data, function(elem) {
+      return elem.cityName.indexOf("臺中市") != -1;
+    });
+    log.info(result);
+    log.timeEnd('timer');
+});
